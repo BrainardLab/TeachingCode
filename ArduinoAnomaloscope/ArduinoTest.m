617d13cae21103eb0d7b70bf80b3c88b85109d51
David Brainard <brainard@psych.upenn.edu>
2020-09-01 20:41:46 -0400

    Adjustment code

--- ArduinoAnomaloscope/ArduinoTest.m
+++ ArduinoAnomaloscope/ArduinoTest.m
@@ -8,16 +8,91 @@ end
 clear a
 a = arduino;
 
-% call to turn on LEDs
-writeYellow(a, "D9", 128);
-writeRGB(a, 255, 255, 0);
+yellow = 150;
+yellowIncr = 1;
 
+redAnchor = 200;
+greenAnchor = 110;
+lambda = 0.5;
+lambdaInc = 0.005;
+redOnly = false;
+greenOnly = false;
+
+% Setup character capture.
+ListenChar(2);
+FlushEvents;
+
+% Loop until the 'q' is pressed.  All other keys cause the rectangle to
+% change color randomly.
+while true
+    red = round(lambda*redAnchor);
+    green = round((1-lambda)*greenAnchor);
+    fprintf('Red = %d, Green = %d, Yellow = %d\n',red, green, yellow);
+    if (redOnly)
+        green = 0;
+    end
+    if (greenOnly)
+        red = 0;
+    end
+    writeRGB(a,red,green,0);
+    writeYellow(a,yellow);
+    
+    switch GetChar
+        case 'q'
+            break;
+            
+        case 'r'
+            lambda = lambda+lambdaInc;
+            if (lambda > 1)
+                lambda = 1;
+            end
+            
+        case 'g'
+            lambda = lambda-lambdaInc;
+            if (lambda < 0)
+                lambda = 0;
+            end
+            
+        case 'i'
+            yellow = round(yellow+yellowIncr);
+            if (yellow > 255)
+                yellow = 255;
+            end
+            
+        case 'd'
+            yellow = round(yellow-yellowIncr);
+            if (yellow < 0)
+                yellow = 0;
+            end
+            
+        case '1'
+            redOnly = true;
+            greenOnly = false;
+            
+        case '2'
+            redOnly = false;
+            greenOnly = true;
+            
+        case '3'
+            redOnly = false;
+            greenOnly = false;
+          
+        otherwise
+            
+    end
+    
 end
+
+% Turn off character capture.
+ListenChar(0);
+
+clear a;
+
 
 
 % %delay for as long as necessary to perform measurements
 % pause;
-% 
+%
 % nLevels = 100;
 % levels = linspace(0,1,nLevels);
 % for jj = 1:5
@@ -30,10 +105,10 @@ end
 %         WaitSecs(1/nLevels);
 %     end
 % end
-% 
+%
 % writeYellow(a, 128);
-% writeRGB(a, 255, 128, 255);  
-%  
+% writeRGB(a, 255, 128, 255);
+%
 % pause;
 
 %clear a
--- ArduinoAnomaloscope/writeYellow.m
+++ ArduinoAnomaloscope/writeYellow.m
@@ -1,7 +1,7 @@
 % Controls the Yellow LED. Takes two arguments: the arduino object and the
 % Y color code to be displayed. The color values should
 % go from 0 to 255, with 255 being the brightest. 
-function writeYellow(a, pwmYellow)
-    pwmYellow =    bytesToPWM(yellow);
+function writeYellow(a, yellow)
+    pwmYellow =    bytesToPWMYellow(yellow);
     writePWMDutyCycle(a, "D9", pwmYellow);
 end
