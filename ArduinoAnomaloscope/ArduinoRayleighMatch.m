7fb89b1812c818a2a8a9fa3cfd3b34c60216e8b9
David Brainard <brainard@psych.upenn.edu>
2022-07-22 08:55:11 -0400

    Paste in alternate key snag code.

--- ArduinoAnomaloscope/ArduinoRayleighMatch.m
+++ ArduinoAnomaloscope/ArduinoRayleighMatch.m
@@ -64,6 +64,26 @@ greenOnly = false;
 ListenChar(2);
 FlushEvents;
 
+% KbName('UnifyKeyNames');
+%
+%     [ keyIsDown, seconds, keyCode ] = KbCheck;
+%     keyCode = find(keyCode, 1);
+% 
+%     % If the user is pressing a key, then display its code number and name.
+%     if keyIsDown
+%         % Note that we use find(keyCode) because keyCode is an array.
+%         % See 'help KbCheck'
+%         fprintf('You pressed key %i which is %s\n', keyCode, KbName(keyCode));
+% 
+%         if keyCode == escapeKey
+%             break;
+%         end
+% 
+%         % If the user holds down a key, KbCheck will report multiple events.
+%         % To condense multiple 'keyDown' events into a single event, we wait until all
+%         % keys have been released.
+%         KbReleaseWait;
+
 % Loop and process characters to control yellow intensity and 
 % red/green mixture
 %
