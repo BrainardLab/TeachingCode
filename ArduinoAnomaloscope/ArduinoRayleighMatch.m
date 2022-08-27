838500be5135b4f560127e8091938e94888f448b
David Brainard <brainard@psych.upenn.edu>
2022-08-27 10:28:16 -0400

    Try to autodetect port

--- ArduinoAnomaloscope/ArduinoRayleighMatch.m
+++ ArduinoAnomaloscope/ArduinoRayleighMatch.m
@@ -24,7 +24,7 @@
 % manner.  Will probably fail on Windows and Linux
 if (~exist('arduinosetup.m','file'))
     if (~strcmp(computer,'MACI64'))
-        error('You need to modify code for Windows/Linux to get the Arduino AddOn Toolbox onto your path');
+        error('You need to modify code for Windows/Linux to get the Arduino AddOn Toolbox onto your path and to get the arduino call to find the device');
     end
     a = ver;
     rel = a(1).Release(2:end-1);
@@ -34,9 +34,45 @@ if (~exist('arduinosetup.m','file'))
 end
 
 % Initialize arduino
+%
+% In newer versions of OS/Matlab, the arduino call without an argument
+% fails because the port naming convention it assumes fails.  
+%
+% We look for possible ports.  If none, we try a straight call to arduino
+% because it might work.  Otherwise we try each port in turn, hoping we 
+% can open the arduino on one of them.
 clear;
 clear a;
-a = arduino;
+devRootStr = '/dev/cu.usbmodem';
+arduinoType = 'leonardo';
+possiblePorts = dir([devRootStr '*']);
+openedOK = false;
+if (isempty(possiblePorts))
+    try 
+        a = arduino;
+        openedOK = true;
+        fprintf('Opened arduino using arduino function''s autodetect of port and type\n');
+    catch e
+        fprintf('Could not detect the arduino port or otherwise open it.\n');
+        fprintf('Rethrowing the underlying error message.\n');
+        rethrow(e);
+    end
+else
+    for pp = 1:length(possiblePorts)
+        thePort = fullfile(possiblePorts.folder,possiblePorts.name);
+        try
+            a = arduino(thePort,arduinoType);
+            openedOK = true;
+        catch e
+        end
+    end
+    if (~openedOK)
+        fprintf('Despite our best cleverness, unable to open arduino. Exiting with an error\n');
+        error('');
+    else
+        fprintf('Opened arduino on detected port %s\n',thePort);
+    end
+end
 
 % Yellow LED parameters
 yellow = 66;                                    % Initial yellow value
